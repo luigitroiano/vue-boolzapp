8f65a65ca52e68116cd22d6687649688d5b1d90e
LuigiTroiano <troiano.luigi8@gmail.com>
2020-12-23 00:50:47 +0100

    Milestone 2 Completata

--- Milestone2/assets/js/main.js
+++ Milestone2/assets/js/main.js
@@ -6,7 +6,7 @@ let app = new Vue ({
         name: "Michele",
         avatar: "_1",
         visible: true,
-        message: [
+        messages: [
           {
             date: "10/01/2020 15:30:55",
             text: "Hai portato a spasso il cane?",
@@ -86,5 +86,11 @@ let app = new Vue ({
         ],
       },
     ],
+    activeContact: 0,
+  },
+  methods: {
+    setActiveContact(index){
+      this.activeContact = index;
+    },
   },
 });
